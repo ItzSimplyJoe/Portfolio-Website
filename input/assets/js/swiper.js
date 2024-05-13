c616cc89df2a4340b197fe8a4b3f7f03220c2bb9
Joe Bostock <joe.bostock@saluscyber.com>
2024-05-13 17:04:01 +0100

    Added skills

---
+++ input/assets/js/swiper.js
@@ -0,0 +1,12 @@
+﻿const swiper = new Swiper('.swiper', {
+    slidesPerView: 6,
+    spaceBetween: 24,
+    loop: true,
+    speed: 5000,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: true,
+        reverseDirection: false,
+        pauseOnMouseEnter: true,
+    },
+});
